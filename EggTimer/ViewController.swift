2b742892cd829d1256d3f8ca9c8426d2deeb2c21
Laith Hanania <laithhanania@Laiths-MacBook-Pro.local>
2022-05-19 17:17:20 -0400

    Add counter functionality

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var counter : Int? = nil
+    var timer = Timer()
+    
+    @IBOutlet weak var titleLabel: UILabel!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle
+        timer.invalidate()
+        titleLabel.text = "How do you like your eggs?"
+        let hardness = sender.currentTitle!
+        counter = eggTimes[hardness]!
         
-        print(eggTimes[hardness!]!)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateCounter() {
+        //example functionality
+        if counter! > 0 {
+            print("\(counter!) seconds to the end of the world")
+            counter! -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done"
+        }
     }
     
 
