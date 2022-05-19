59a8203b2a42b6b5ef3996a8dda55c0e10cd4e6f
Laith Hanania <laithhanania@Laiths-MacBook-Pro.local>
2022-05-19 17:54:12 -0400

    Add progress bar functionality and alarm

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,33 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-    
+    var player: AVAudioPlayer?
     let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
     var counter : Int? = nil
     var timer = Timer()
+    var hardness = "Soft"
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progress: UIProgressView!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         timer.invalidate()
         titleLabel.text = "How do you like your eggs?"
-        let hardness = sender.currentTitle!
+        hardness = sender.currentTitle!
         counter = eggTimes[hardness]!
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
-        
     }
     
     @objc func updateCounter() {
         //example functionality
         if counter! > 0 {
+            progress.progress = Float(1 - (Double(counter!) / Double(eggTimes[hardness]!)))
             print("\(counter!) seconds to the end of the world")
             counter! -= 1
         } else {
             timer.invalidate()
             titleLabel.text = "Done"
+            progress.progress = 1
+            guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+            player = try! AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            player!.play()
+
         }
     }
     
